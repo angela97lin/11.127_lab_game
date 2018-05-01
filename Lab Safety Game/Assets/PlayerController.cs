b761d83c4630c06e981282d38442b8ee35180bbe
Jenny Xu <jennycxu@mit.edu>
2018-05-01 17:54:37 -0400

    added basic scene setup for the bunsen scene and animations

---
+++ Lab Safety Game/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		var vertical = Input.GetAxis("Vertical");
+		var horizontal = Input.GetAxis("Horizontal");
+
+		if (vertical > 0)
+		{
+			animator.SetInteger("State", 1);
+		}
+		else if (vertical < 0)
+		{
+			animator.SetInteger("State", 2);
+		}
+		else if (horizontal > 0)
+		{
+			animator.SetInteger("State", 1);
+		}
+		else if (horizontal < 0)
+		{
+			animator.SetInteger("State", 0);
+		}
+	}
+}
