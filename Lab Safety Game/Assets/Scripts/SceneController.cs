fac4621ec3e67801c51a716db3470da5ecf4ae1b
Jenny Xu <jennycxu@mit.edu>
2018-05-03 09:12:51 -0400

    added scenes and a main screen to navigate

---
+++ Lab Safety Game/Assets/Scripts/SceneController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+			if (hit.collider != null) {
+				switch (hit.collider.gameObject.name) {
+				case "Bean":
+					SceneManager.LoadScene (2);
+					break;
+				case "beaker":
+					SceneManager.LoadScene (0);
+					break;
+				case "BeanFile":
+					SceneManager.LoadScene (1);
+					break;
+				default:
+					break;
+				}
+
+			}
+		}
+
+	}
+}
