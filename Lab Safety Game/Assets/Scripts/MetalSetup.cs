1c3fc3e79c90d06b151035df7e144b8107661710
Angela Lin <ange@mit.edu>
2018-05-14 15:05:03 -0400

    pushy

---
+++ Lab Safety Game/Assets/Scripts/MetalSetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalSetup : MonoBehaviour {
+
+	public GameObject 
+	public GameObject 
+	public GameObject 
+	public GameObject 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
